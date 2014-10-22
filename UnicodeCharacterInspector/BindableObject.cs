e1f5c1c471a7c84feae41a6b1d07ce2bccba528f
GoldenCrystal <GoldenCrystal@users.noreply.github.com>
2014-10-23 01:54:48 +0200

    Initial commit.

---
+++ UnicodeCharacterInspector/BindableObject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnicodeCharacterInspector
+{
+	internal abstract class BindableObject : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			var handler = PropertyChanged;
+
+			if (handler != null)
+				handler(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		protected bool SetValue<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+		{
+			if (!EqualityComparer<T>.Default.Equals(value, storage))
+			{
+				storage = value;
+				NotifyPropertyChanged(propertyName);
+				return true;
+			}
+			return false;
+		}
+
+		protected bool SetValue<T>(ref T storage, T value, IEqualityComparer<T> equalityComparer, [CallerMemberName] string propertyName = null)
+		{
+			if (!(equalityComparer ?? EqualityComparer<T>.Default).Equals(value, storage))
+			{
+				storage = value;
+				NotifyPropertyChanged(propertyName);
+				return true;
+            }
+			return false;
+		}
+	}
+}
---
+++ UnicodeCharacterInspector/CharacterInfoViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnicodeCharacterInspector
+{
+	internal sealed class CharacterInfoViewModel : BindableObject
+	{
+		private string character;
+		private int codePoint;
+		private UnicodeCategory category = CharUnicodeInfo.GetUnicodeCategory('\0');
+
+		public CharacterInfoViewModel()
+		{
+		}
+
+		public string Character
+		{
+			get { return character; }
+			set
+			{
+				if (value != null)
+				{
+					if (value.Length == 0)
+					{
+						value = null;
+					}
+					else if (value.Length > 2 || value.Length == 2 && !char.IsSurrogatePair(value, 0))
+					{
+						throw new ArgumentException();
+					}
+				}
+
+				if (value != character)
+				{
+					if ((character = value) != null)
+					{
+						codePoint = char.ConvertToUtf32(character, 0);
+						category = CharUnicodeInfo.GetUnicodeCategory(character, 0);
+					}
+					else
+					{
+						codePoint = 0;
+						category = CharUnicodeInfo.GetUnicodeCategory('\0');
+                    }
+
+					NotifyPropertyChanged();
+					NotifyPropertyChanged("CodePoint");
+					NotifyPropertyChanged("Category");
+				}
+			}
+		}
+
+		public int? CodePoint
+		{
+			get { return character != null ? codePoint : null as int?; }
+		}
+
+		public UnicodeCategory? Category
+		{
+			get { return character != null ? category : null as UnicodeCategory?; }
+		}
+	}
+}
