d7ada9e413e16eaceab9fd1f3bc496b95d563407
GoldenCrystal <GoldenCrystal@users.noreply.github.com>
2014-11-14 02:19:53 +0100

    Display the code points next to the glyphs in the list, and tweak the UI a bit.

---
+++ UnicodeCharacterInspector/StringToUtf32Converter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace UnicodeCharacterInspector
+{
+	internal class StringToUtf32Converter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			string text = value as string;
+
+			if (!string.IsNullOrEmpty(text)) return char.ConvertToUtf32(text, 0);
+			else return null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
