6db33156ce59d7f8c1de1ef101dd31ee5f84a534
GoldenCrystal <8518235+GoldenCrystal@users.noreply.github.com>
2020-03-15 01:00:11 +0100

    Better comment the Unihan code point packing methods.

--- System.Unicode/UnihanCharacterData.cs
+++ System.Unicode/UnihanCharacterData.cs
@@ -51,31 +51,54 @@ namespace System.Unicode
 			TraditionalVariant = traditionalVariant;
 		}
 
+		// This method packs code points by predicted order of importance (it may be wrong)
+		// Its purpose is to avoid skipping numbers so that file encoding can be more efficient.
 		internal static int PackCodePoint(int codePoint)
 		{
 			if (codePoint >= 0x3400)
 			{
+				// 3400..4DBF; CJK Unified Ideographs Extension A
+				// 4DC0..4DFF; Yijing Hexagram Symbols
 				if (codePoint < 0x4E00) return codePoint + 0x1E00;
+				// 4E00..9FFF; CJK Unified Ideographs
 				else if (codePoint < 0xA000) return codePoint - 0x4E00;
+				// F900..FAFF; CJK Compatibility Ideographs
 				else if (codePoint >= 0xF900 && codePoint < 0xFB00) return codePoint + 0xFD00;
 				else if (codePoint >= 0x20000)
 				{
+					// 20000..2A6DF; CJK Unified Ideographs Extension B
+					// 2A700..2B73F; CJK Unified Ideographs Extension C
+					// 2B740..2B81F; CJK Unified Ideographs Extension D
+					// 2B820..2CEAF; CJK Unified Ideographs Extension E
+					// 2CEB0..2EBEF; CJK Unified Ideographs Extension F
 					if (codePoint < 0x2F800) return codePoint - 0x19400;
-					else if (codePoint < 0x30000) return codePoint - 0x10000;
+					// 2F800..2FA1F; CJK Compatibility Ideographs Supplement
+					else if (codePoint < 0x2FA20) return codePoint - 0x10000;
 				}
 			}
 
 			throw new ArgumentOutOfRangeException(nameof(codePoint));
 		}
 
+		// Reverses the packing done by the PackCodePoint method.
 		internal static int UnpackCodePoint(int packedCodePoint)
 		{
 			if (packedCodePoint >= 0)
 			{
+				// 4E00..9FFF; CJK Unified Ideographs
 				if (packedCodePoint < 0x05200) return packedCodePoint + 0x4E00;
+				// 3400..4DBF; CJK Unified Ideographs Extension A
+				// 4DC0..4DFF; Yijing Hexagram Symbols
 				else if (packedCodePoint < 0x06C00) return packedCodePoint - 0x1E00;
+				// 20000..2A6DF; CJK Unified Ideographs Extension B
+				// 2A700..2B73F; CJK Unified Ideographs Extension C
+				// 2B740..2B81F; CJK Unified Ideographs Extension D
+				// 2B820..2CEAF; CJK Unified Ideographs Extension E
+				// 2CEB0..2EBEF; CJK Unified Ideographs Extension F
 				else if (packedCodePoint < 0x1F600) return packedCodePoint + 0x19400;
+				// F900..FAFF; CJK Compatibility Ideographs
 				else if (packedCodePoint < 0x1F800) return packedCodePoint - 0xFD00;
+				// 2F800..2FA1F; CJK Compatibility Ideographs Supplement
 				else if (packedCodePoint < 0x20000) return packedCodePoint + 0x10000;
 			}
 			throw new ArgumentOutOfRangeException(nameof(packedCodePoint));
