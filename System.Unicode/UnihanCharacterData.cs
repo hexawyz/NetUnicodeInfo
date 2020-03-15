c922a66e64003a1a89daae900249b320330b6ace
GoldenCrystal <8518235+GoldenCrystal@users.noreply.github.com>
2020-03-15 01:04:07 +0100

    Exlude Yijing Hexagram Symbols from Unihan code point packing.

--- System.Unicode/UnihanCharacterData.cs
+++ System.Unicode/UnihanCharacterData.cs
@@ -58,22 +58,24 @@ namespace System.Unicode
 			if (codePoint >= 0x3400)
 			{
 				// 3400..4DBF; CJK Unified Ideographs Extension A
-				// 4DC0..4DFF; Yijing Hexagram Symbols
-				if (codePoint < 0x4E00) return codePoint + 0x1E00;
-				// 4E00..9FFF; CJK Unified Ideographs
-				else if (codePoint < 0xA000) return codePoint - 0x4E00;
-				// F900..FAFF; CJK Compatibility Ideographs
-				else if (codePoint >= 0xF900 && codePoint < 0xFB00) return codePoint + 0xFD00;
-				else if (codePoint >= 0x20000)
+				if (codePoint < 0x4DC0) return codePoint + 0x1E00;
+				else if (codePoint >= 0x4E00)
 				{
-					// 20000..2A6DF; CJK Unified Ideographs Extension B
-					// 2A700..2B73F; CJK Unified Ideographs Extension C
-					// 2B740..2B81F; CJK Unified Ideographs Extension D
-					// 2B820..2CEAF; CJK Unified Ideographs Extension E
-					// 2CEB0..2EBEF; CJK Unified Ideographs Extension F
-					if (codePoint < 0x2F800) return codePoint - 0x19400;
-					// 2F800..2FA1F; CJK Compatibility Ideographs Supplement
-					else if (codePoint < 0x2FA20) return codePoint - 0x10000;
+					// 4E00..9FFF; CJK Unified Ideographs
+					if (codePoint < 0xA000) return codePoint - 0x4E00;
+					// F900..FAFF; CJK Compatibility Ideographs
+					else if (codePoint >= 0xF900 && codePoint < 0xFB00) return codePoint + 0xFD00;
+					else if (codePoint >= 0x20000)
+					{
+						// 20000..2A6DF; CJK Unified Ideographs Extension B
+						// 2A700..2B73F; CJK Unified Ideographs Extension C
+						// 2B740..2B81F; CJK Unified Ideographs Extension D
+						// 2B820..2CEAF; CJK Unified Ideographs Extension E
+						// 2CEB0..2EBEF; CJK Unified Ideographs Extension F
+						if (codePoint < 0x2F800) return codePoint - 0x19400;
+						// 2F800..2FA1F; CJK Compatibility Ideographs Supplement
+						else if (codePoint < 0x2FA20) return codePoint - 0x10000;
+					}
 				}
 			}
 
@@ -88,7 +90,6 @@ namespace System.Unicode
 				// 4E00..9FFF; CJK Unified Ideographs
 				if (packedCodePoint < 0x05200) return packedCodePoint + 0x4E00;
 				// 3400..4DBF; CJK Unified Ideographs Extension A
-				// 4DC0..4DFF; Yijing Hexagram Symbols
 				else if (packedCodePoint < 0x06C00) return packedCodePoint - 0x1E00;
 				// 20000..2A6DF; CJK Unified Ideographs Extension B
 				// 2A700..2B73F; CJK Unified Ideographs Extension C
