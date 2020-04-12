dde584d83c674c049b373828dbf7304cf8c235c5
GoldenCrystal <8518235+GoldenCrystal@users.noreply.github.com>
2020-04-12 18:03:16 +0200

    Add unit tests for Unihan code point packing.

---
+++ System.Unicode.Tests/UnihanCharacterDataTests.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using Xunit;
+
+namespace System.Unicode.Tests
+{
+	public sealed class UnihanCharacterDataTests
+	{
+		private static readonly UnicodeBlock[] Blocks = UnicodeInfo.GetBlocks();
+
+		[Theory]
+		[InlineData("CJK Unified Ideographs")]
+		[InlineData("CJK Unified Ideographs Extension A")]
+		[InlineData("CJK Unified Ideographs Extension B")]
+		[InlineData("CJK Unified Ideographs Extension C")]
+		[InlineData("CJK Unified Ideographs Extension D")]
+		[InlineData("CJK Unified Ideographs Extension E")]
+		[InlineData("CJK Unified Ideographs Extension F")]
+		[InlineData("CJK Compatibility Ideographs")]
+		[InlineData("CJK Compatibility Ideographs Supplement")]
+		public void CodePointPackingShouldRoundTrip(string blockName)
+		{
+			var block = Blocks.Single(b => b.Name == blockName);
+
+			foreach (int codePoint in block.CodePointRange)
+			{
+				Assert.Equal(codePoint, UnihanCharacterData.UnpackCodePoint(UnihanCharacterData.PackCodePoint(codePoint)));
+			}
+		}
+	}
+}
